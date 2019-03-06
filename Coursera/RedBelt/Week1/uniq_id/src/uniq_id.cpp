ebfa99196c66916551329fb34eb4cb4edf0326cf
Dmitriy Kovalenko <kovalenkodmitrij@gmail.com>
2019-03-06 14:00:09 +0200

    uniq_id.cpp Ok

---
+++ Coursera/RedBelt/Week1/uniq_id/src/uniq_id.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+#define TOKENPASTE(y) x##y
+#define TOKENPASTE2( y) TOKENPASTE( y)
+#define UNIQ_ID TOKENPASTE2(__LINE__)
+
+
+
+int main() {
+  int UNIQ_ID = 0;
+  string UNIQ_ID = "hello";
+  vector<string> UNIQ_ID = {"hello", "world"};
+  vector<int> UNIQ_ID = {1, 2, 3, 4};
+
+}
