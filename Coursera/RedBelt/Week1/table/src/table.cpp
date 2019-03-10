2dee165ed996c605b1e533dc627c744b394233fd
Dmitriy Kovalenko <kovalenkodmitrij@gmail.com>
2019-03-10 18:58:18 +0200

    table.cpp Ok

--- Coursera/RedBelt/Week1/table/src/table.cpp
+++ Coursera/RedBelt/Week1/table/src/table.cpp
@@ -6,52 +6,52 @@ using namespace std;
 
 
 
+
 // Реализуйте здесь шаблонный класс Table
 template <class T>
 class Table{
 	public:
 	Table(size_t row_, size_t col_): m_row(row_), m_col(col_){
-		arr = new T * [m_row];		//создаём двумерный массив
-			for (unsigned i = 0; i < m_row; i++) {
-				arr[i] = new T[m_col];
-			}
+		Resize(row_, col_);
 	}
 
-	T* operator[]( const size_t& index) {
-	    return arr[index];
-	  }
 
-	  const T* operator[](size_t index) const {
-	    return arr[index];
-	  }
-	  void Resize(const size_t& x=0, const size_t& y=0){
-		  m_row =x;
-		  m_col = y;
-		  T** arr_temp;
-		  arr_temp= new T*[m_row];
-		  for(size_t i=0; i < m_row; i++){
-			  arr_temp[i]= new T[m_col];
-		  }
-		  for(size_t x=0; x<m_row-1; x++){
-			  for(size_t y=0; y<m_col-1; y++){
-				  arr_temp[x][y]= arr[x][y];
-			  }
-		  }
-		  cout << "test" << arr_temp[1][1]<< endl;
-		  //copy(arr, arr+min(m_row, m_col),arr_temp);
-		  delete[] arr;
-		  arr=arr_temp;
+	  vector<T>& operator [] (size_t row) {
 
-	  }
+
+	        return matrix[row];
+
+	    }
+
+
+
+	  const vector<T>& operator [] (size_t row) const {
+
+
+	  	        return matrix[row];
+
+	  	    }
+
+
+
+	  void Resize(const size_t& row, const size_t& col){
+		 matrix.resize(row);
+		 for(auto& i: matrix){
+			 i.resize(col);
+		 }
+
+	 }
+
+
 
 	  pair<size_t, size_t> Size() const {
-		       return {m_row, m_col};
+		       return {matrix.size(), (matrix.empty() ? 0 : matrix[0].size())};
 	  }
 
 
 private:
 	size_t m_row, m_col;
-	T** arr;
+	vector<vector<T>> matrix;
 };
 
 
@@ -59,23 +59,41 @@ void TestTable() {
   Table<int> t(1, 1);
   ASSERT_EQUAL(t.Size().first, 1u);
   ASSERT_EQUAL(t.Size().second, 1u);
- t[0][0] = 42;
+  t[0][0] = 42;
   ASSERT_EQUAL(t[0][0], 42);
-//  t.Resize(3, 4);
-// ASSERT_EQUAL(t.Size().first, 3u);
- // ASSERT_EQUAL(t.Size().second, 4u);
+  t.Resize(3, 4);
+  ASSERT_EQUAL(t.Size().first, 3u);
+  ASSERT_EQUAL(t.Size().second, 4u);
 }
 
 int main() {
-
-	Table<int> tt(2,2);
-	tt[1][1] = 4;
-	cout << tt[1][1];
+/*
+	Table<int> tt(3,5);
+	tt[2][3] = 4;
+	cout << "tt[1][0] =" << tt[1][0];
 	cout << endl;
-	tt.Resize(5,6);
-	cout << tt[1][1];
+	for(int i=0; i<3; ++i){
+			for(int y=0; y<5; ++y){
+				cout<< "tt[" << i << "][" << y << "]= " << tt[i][y] << endl;
+		}
+	}
 
+	tt.Resize(0,0);
+	cout <<"resize 00"<< endl;
+	for(int i=0; i<8; ++i){
+			for(int y=0; y<4; ++y){
+				cout<< "tt[" << i << "][" << y << "]= " << tt[i][y] << endl;
+		}
+	}
+			tt.Resize(8,9);
+				cout <<"resize 8 9"<< endl;
+				for(int i=0; i<8; ++i){
+						for(int y=0; y<9; ++y){
+							cout<< "tt[" << i << "][" << y << "]= " << tt[i][y] << endl;
+					}
+	}
 
+*/
   TestRunner tr;
   RUN_TEST(tr, TestTable);
   return 0;
--- Coursera/RedBelt/Week1/test/src/test.cpp
+++ Coursera/RedBelt/Week1/test/src/test.cpp
@@ -1,72 +1,28 @@
-//Листинг #3        Перегрузка [][]             Используем объект класса как двумерный массив       Visual Studio 2015
+#include <vector>
  #include <iostream>
 
-using std::cout;
-using std::cin;
 
-class MyArr {
-	unsigned m_row;			//количество строк
-	unsigned m_col;			//количество колонок
-	int **arr;				//указательная переменная в качестве массива
-	MyArr() {}
-public:
-	MyArr(const unsigned, const unsigned);		//прототип конструктора с параметрами, для количеств на массив
-	~MyArr();		//прототип деструктора
+using namespace std;
 
-	unsigned row() const { return m_row; }		//функция для вытаскивания числа строк
-	unsigned col() const { return m_col; }		//функция для вытаскивания числа строк
-
-	int* operator[](const int i) {
-		return arr[i];
+template<class It, class T>
+struct Row{
+	It I;
+	T& operator[](size_t index){
+		return *next(I, index);
 	}
 
-	const int* operator[](const int i) const {
-		return arr[i];
-	}
 };
 
-MyArr::MyArr(const unsigned row_, const unsigned col_):m_row(row_),m_col(col_) {
-	arr = new int * [m_row];		//создаём двумерный массив
-	for (unsigned i = 0; i < m_row; i++) {
-		arr[i] = new int[m_col];
-	}
-}
-
-
-MyArr:: ~MyArr() {
-	/*зачищаем двумерный массив*/
-	for (unsigned i = 0; i < m_row; i++) {
-		delete[]arr[i];
-	}
-
-	delete[]arr;
-}
-
-
-
-
 int main() {
-	MyArr arr(10, 6);
-
-	unsigned count = 0;	//для заполнения по порядку
-
-	for (unsigned i = 0; i < arr.row(); i++) {
-		for (unsigned j = 0; j < arr.col(); j++) {
-			arr[i][j] = count++;
-		}
+	vector<int> v ={1,1,1,3,3,3,4,4,4};
+	Row<typename vector<int>::iterator, int> aaa;
+	auto it = v.begin();
+	aaa.I=v.begin();
+	for(int x=0; x<v.size(); ++x){
+	cout << "aaa[" << "x" <<"]= " << aaa[x] << endl;
 	}
 
-	for (unsigned i = 0; i < arr.row(); i++) {
-		for (unsigned j = 0; j < arr.col(); j++) {
-			cout << arr[i][j] << '\t';
-		}
-		cout << "\n";
-	}
 
-	const MyArr arr2(10, 6);
-   //arr[0][0] = 100               // для константого объекта нельзя
-
-	cin.get();
 }
 
 
