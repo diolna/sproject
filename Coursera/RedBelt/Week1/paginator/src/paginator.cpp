03949a2e28a5a8fff19baedd1e306ee49052115e
Dmitriy Kovalenko <kovalenkodmitrij@gmail.com>
2019-03-12 18:09:33 +0200

    paginator.cpp nOk not work eclipse

--- Coursera/RedBelt/Week1/paginator/src/paginator.cpp
+++ Coursera/RedBelt/Week1/paginator/src/paginator.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
 
 using namespace std;
 
@@ -8,32 +9,76 @@ using namespace std;
 template<typename Iterator>
 class Paginator{
 public:
-	Paginator(const Iterator& begin, const Iterator& end, const size_t& page_size_){}
-	Iterator begin(){return first;};
-	Iterator end(){return last;};
-	size_t size() const {return first-last/page_size;}
+	Paginator(Iterator begin, Iterator end, const size_t& page_size_)
+		: first(begin), last(end), page_size(page_size_)
+		{}
+	Iterator begin() const {return first;};
+	Iterator end() const {return last;};
+	size_t size() const {return distance(first,last);}
+
+	vector<Paginator> SetVector() {
+		cout << "this->size() = " << this->size()<< endl;
+		if(this->size()<page_size){
+				vector<Paginator> v={{first,first+this->size(),page_size},
+								{first+this->size(), last}};
+			return v;
+		}else if(this->size()==page_size){
+				cout << "==" << endl;
+				return {{first, last, page_size}};
+		}else{
+			vector<Paginator> v;
+			int n_page = this->size()/page_size;
+			for(int i=0; i<n_page; ++i){
+				if(first+page_size< last){
+					v.push_back({first, first+page_size, page_size});
+					first = first+page_size;
+				}else{
+				v.push_back({first, last, page_size});
+			}
+		}
+
+		//vector<Paginator> v={{first,last,page_size},{first+2,last-1,page_size}};
+		//return v;
+	}
+	}
 
 private:
 	Iterator first, last;
 	size_t page_size;
 
 };
-template <typename C>
- auto Paginate(C& c, size_t page_size){
-		return {c.begin(), c.end()};
-}
 
-vector<vector<Application>> DistributeAmongScreens(const vector<Application>& apps) {
-  vector<vector<Application>> result;
-  for (const auto& page : Paginate(apps, 20)) {
-    result.push_back({page.begin(), page.end()});
-  }
+template <typename C>
+  auto Paginate(C& c, size_t page_size){
+		Paginator pag(c.begin(), c.end(), page_size);
+		//vector<C> v;
+		return pag.SetVector();
+}
+//vect:ctor<vector<int>> result;
+//  for (const auto& page : Paginate(apps, 20)) {
+//    result.push_back({page.begin(), page.end()});
+ // }
   // result[0] - все приложения, которые попадают на первый экран,
   // result[1] - все приложения, которые попадают на второй экран и т.д.
-  return result;
-}
+//  return result;
+//}
 
 int main() {
+	cout << "test8" << endl;
+	vector<int> v = {1,2,3,4,5,6};
+	vector<vector<int>> res;
+	int count =0;
+	for(auto& i: Paginate(v,6)){
+		res.push_back({i.begin(), i.end()});
+		count++;
+		cout << "number vector  " << count<< "=" ;
+		for(auto& x: i){
+
+			cout << " "<< x << " ";
+		}
+		cout << endl;
+	}
+//fgfgfgfgfgfgfdgfd
 
 	return 0;
 }
--- Coursera/RedBelt/Week1/video_iter_user_class_in_for/src/video_iter_user_class_in_for.cpp
+++ Coursera/RedBelt/Week1/video_iter_user_class_in_for/src/video_iter_user_class_in_for.cpp
@@ -5,34 +5,55 @@ using namespace std;
 
 template<class Iterator>					//шаблонный класс обь€вл€ет два итератора
 struct IteratorRange{
+private:
 	Iterator first, last;
+	int a;
+public:
+	IteratorRange(Iterator f, Iterator l, int a_): first(f), last(l), a(a_){}
 
 	Iterator begin() const {return first;}	// необходимо €вно определить методы дл€ begin и end
 	Iterator end() const {return last;}
 };
 
-
+/*
 
 template <class T>
-IteratorRange<class vector<T>::iterator> Head(    // инстанцирование создание конкретного типа
+IteratorRange<typename vector<T>::iterator> Head(    // инстанцирование создание конкретного типа
 		vector<T>& v, size_t top){					// из шаблона класса
 	return {
 		v.begin(), next(v.begin(), min(top, v.size()))
 	};
 }
 
-
+*/
+//template <typename Iterator>
+//IteratorRange<Iterator> MakeRange(Iterator begin, Iterator end){
+//	return IteratorRange<Iterator>{begin, end};
+//}
 
 int main() {
 
+	cout << "test1 " << endl;
+	pair p(4,true);
+
+
+
 	vector<int> v ={1,2,3,4,5};
 
+	//auto second_half =MakeRange(v.begin()+v.size()/2, v.end());
+	IteratorRange second_half2(v.begin()+v.size()/2, v.end()-1,3);
+
+	for(auto& i: second_half2){
+		cout << i << " ";
+	}
+	cout << endl;
+/*
 	for(auto& i: Head(v,3)){        // ¬ызов шаблонной функции дл€ итерировани€ до 3 шага.
 		++i;						// возвращает итератор на 3 элемент.
 	}
 	for (auto i: v){
 		cout << i << " ";
 	}
-
+*/
 	return 0;
 }
