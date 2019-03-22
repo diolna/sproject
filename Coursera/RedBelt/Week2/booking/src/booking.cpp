60b9b48049f93b112ff52cb0298fd5dcca7544a3
Dmitriy Kovalenko <kovalenkodmitrij@gmail.com>
2019-03-22 18:26:36 +0200

    booking.cpp nok

--- Coursera/RedBelt/Week2/booking/src/booking.cpp
+++ Coursera/RedBelt/Week2/booking/src/booking.cpp
@@ -2,40 +2,57 @@
 #include <string>
 #include <map>
 #include <set>
+#include <numeric>
+#include <vector>
 
 using namespace std;
 
 class Hotels {
 public:
-	void BOOK(const int& t, const string& name_hotel, const int& client_id);
+	void BOOK(const int& time, const string& name_hotel, const int& client_id, const int& room_count);
 	int CLIENTS(const string& name_hotel);
 	int ROOMS(const string& name_hotel);
 
 private:
-	map<string, set<int>> day_clients_;
-	map<string, map<int, int>> hotels_; 	// в отеле забронировали номера клиенты
-	map<string, set<int>> client_time_;
+	map<string, map<int, set<int>>> time_; 	// в отеле забронировали номера клиенты
+	map<string, map<int, int>> rooms_;
+	map<string, vector<int>> clients_in_hotel_day;
+	int current_time_;
 };
 
-void Hotels::BOOK(const int& time, const string& name, const int& client) {
+void Hotels::BOOK(const int& time, const string& name_hotel, const int& client, const int& room_count) {
+			time_[name_hotel][time].insert(client);
+			rooms_[name_hotel][client] = room_count;
+			map<string, map <int, set<int>>>::iterator it = time_.begin();
+			//map<string, map<int, int>>:: iterator it2 = lower_bound(it->second.end()->first);
+
+
 
 }
 
 int Hotels::CLIENTS(const string& name){
-		if( hotels_.at(name).size()==0 ){
-			return 0;
-		} else {
-				return day_clients_.size();
 
-		}
+				return 0;
+
+
 }
 int Hotels::ROOMS(const string& name){
-
+	return 0;
 }
 
 
 
 int main() {
+
+	map<int, map<int, int>> aaa = { {1,{{1,2}}}, {2,{{2,3}}}, {3,{{3,4}}}};
+	map<int,map<int,int>>::iterator it = aaa.find(1);
+	int nnn = it->first;
+	int nnn1 = it->second.begin()->first;
+	int nnn2 = it->second.begin() -> second;
+	cout << nnn << " " << nnn1 << " " << nnn2;
+
+
+
 	// Для ускорения чтения данных отключается синхронизация
 	  // cin и cout с stdio,
 	  // а также выполняется отвязка cin от cout
@@ -54,10 +71,10 @@ int main() {
 	    cin >> user_id;
 
 	    if (query_type == "BOOK") {
-	      int time_booking, client_id;
+	      int time_booking, client_id, rooms_count;
 	      string hotel_name;
-	      cin >> time_booking >> hotel_name >> client_id;
-	      h.BOOK(time_booking, hotel_name, client_id);
+	      cin >> time_booking >> hotel_name >> client_id >> rooms_count;
+	      h.BOOK(time_booking, hotel_name, client_id, rooms_count);
 	    } else if (query_type == "CLIENTS") {
 	    		string hotel_name;
 	    		cout << h.CLIENTS(hotel_name) << endl;
