d914ddb7557392aa036f6dccdda91e189184b5df
Dmitriy Kovalenko <kovalenkodmitrij@gmail.com>
2019-03-22 23:34:59 +0200

    musor

--- Coursera/RedBelt/Week2/booking/src/booking.cpp
+++ Coursera/RedBelt/Week2/booking/src/booking.cpp
@@ -2,38 +2,49 @@
 #include <string>
 #include <map>
 #include <set>
+#include <deque>
 
 using namespace std;
 
-class Hotels {
+
+class Booking {
+
 public:
-	void BOOK(const int& t, const string& name_hotel, const int& client_id);
+	void BOOK(const int& t, const string& name_hotel
+			, const int& client_id, const int& room_c);
 	int CLIENTS(const string& name_hotel);
 	int ROOMS(const string& name_hotel);
 
 private:
-	map<string, set<int>> day_clients_;
-	map<string, map<int, int>> hotels_; 	// в отеле забронировали номера клиенты
-	map<string, set<int>> client_time_;
+	map<string, map<int, map<int, int>>> book;
+	map<string, deque<int>> clients_day_;
+
 };
 
-void Hotels::BOOK(const int& time, const string& name, const int& client) {
+void Booking::BOOK(const int& time, const string& name_hotel
+			, const int& client_id, const int& room_c) {
 
+		if(book.count(name_hotel)==0){
+			book.at(name_hotel).at(client_id).at(room_c) = time;
+		}
 }
 
-int Hotels::CLIENTS(const string& name){
-		if( hotels_.at(name).size()==0 ){
+int Booking::CLIENTS(const string& name){
+		if( book.at(name).size()==0 ){
 			return 0;
 		} else {
-				return day_clients_.size();
+				return clients_day_.size();
 
 		}
 }
-int Hotels::ROOMS(const string& name){
+int Booking::ROOMS(const string& name){
+	if( book.at(name).size()==0 ){
+				return 0;
+			} else {
+					return clients_day_.size();
 
+			}
 }
-
-
 
 int main() {
 	// Для ускорения чтения данных отключается синхронизация
@@ -42,7 +53,7 @@ int main() {
 	  ios::sync_with_stdio(false);
 	  cin.tie(nullptr);
 
-	  Hotels h;
+	  Booking h;
 
 	  int query_count;
 	  cin >> query_count;
@@ -54,10 +65,10 @@ int main() {
 	    cin >> user_id;
 
 	    if (query_type == "BOOK") {
-	      int time_booking, client_id;
+	      int time_booking, client_id, room_c;
 	      string hotel_name;
-	      cin >> time_booking >> hotel_name >> client_id;
-	      h.BOOK(time_booking, hotel_name, client_id);
+	      cin >> time_booking >> hotel_name >> client_id >> room_c;
+	      h.BOOK(time_booking, hotel_name, client_id, room_c);
 	    } else if (query_type == "CLIENTS") {
 	    		string hotel_name;
 	    		cout << h.CLIENTS(hotel_name) << endl;
