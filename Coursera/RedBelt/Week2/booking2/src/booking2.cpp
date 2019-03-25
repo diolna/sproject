c89866f49031a75333c11a8507feefbeda630c90
Dmitriy Kovalenko <kovalenkodmitrij@gmail.com>
2019-03-25 18:01:23 +0200

    booking.cpp 6/17 and booking2.cpp nok

--- Coursera/RedBelt/Week2/booking2/src/booking2.cpp
+++ Coursera/RedBelt/Week2/booking2/src/booking2.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include "test_runner.h"
 #include <cstdint>
+#include <queue>
 
 using namespace std;
 
@@ -22,7 +23,9 @@ public:
 
 private:
 	map<string, map<int64_t, set<int64_t>>> time_; 	// в отеле забронировали  в время клиенты
-	map<string, map<int64_t, vector<int64_t>>> rooms_; // в отеле забронировали в время комнат
+	map<string, map<int64_t, queue<int64_t>>> rooms_; // в отеле забронировали в время комнат
+	map<string, queue<int64_t>> rooms1_;
+	map<string, pair<int64_t, int64_t>> time1_;
 
 };
 
@@ -31,20 +34,28 @@ private:
 void Booking::BOOK(const int64_t& time, const string& name_hotel
 					, const int64_t& client, const int64_t& room_count) {
 
+
+
 					time_[name_hotel][time].insert(client);
-					rooms_[name_hotel][time].push_back(room_count);
+					for(auto i=0; i < room_count; ++i){
+					rooms_[name_hotel][time].push(1);
+					}
 
 					for(auto& i: time_){
 						if(time - i.second.begin()->first >= 86400 )  {
 							int64_t time_del = time  - 86400;
 							auto it_clients = i.second.upper_bound(time_del);
 							i.second.erase(i.second.begin(), it_clients);
+
 						}
 					}
 					for(auto& i: rooms_){
 						if(time - i.second.begin()->first >= 86400 )  {
 								int64_t time_del = time  - 86400;
 								auto it_clients = i.second.upper_bound(time_del);
+								for(auto it = i.second.begin();  it!= it_clients; ++it){
+										rooms1_[i.first].pop();
+															}
 								i.second.erase(i.second.begin(), it_clients);
 						}
 					}
@@ -57,45 +68,43 @@ void Booking::BOOK(const int64_t& time, const string& name_hotel
 
 int64_t Booking::CLIENTS(const string& name_hotel){
 	set<int64_t> clients1;
+		if(time_.count(name_hotel) == 0 ) {
 
-	if(time_.count(name_hotel) == 0 ) {
+			return 0;
+		}
+		if(time_.find(name_hotel)->second.size()==0 ) {
+			return 0;
+		}
+		for(auto& i: time_.find(name_hotel)->second) {
 
-		return 0;
-	}
-	if(time_.find(name_hotel)->second.size()==0 ) {
-		return 0;
-	}
-	for(auto& i: time_.find(name_hotel)->second) {
-
-		clients1.insert(i.second.begin(), i.second.end());
-
+			clients1.insert(i.second.begin(), i.second.end());
 
 
-	}
-
-	return clients1.size();
+		}
+		return clients1.size();
 }
 
 int64_t Booking::ROOMS(const string& name_hotel){
 
 	int64_t rooms = 0;
-		if(rooms_.count(name_hotel) == 0 ) {
+	if(rooms_.count(name_hotel) == 0 ) {
 
-			return 0;
-		}
-		if(rooms_.find(name_hotel) ->second.size()==0){
-			return 0;
-		}
-			auto it = rooms_.find(name_hotel);
+		return 0;
+	}
+	if(rooms_.find(name_hotel) ->second.size()==0){
+		return 0;
+	}
+		auto it = rooms_.find(name_hotel);
 
-			map<int64_t, vector<int64_t>> v;
-			v = it->second;
-			for(auto& i: v){
-
-				rooms += accumulate(i.second.begin(), i.second.end(),0);
+		map<int64_t, queue<int64_t>> v;
+		v = it->second;
+		cout << v.size();
+		for(int64_t y=0; y < v.size(); ++y){
+				rooms += v[y];
 			}
 
-		return rooms;
+
+	return rooms;
 }
 
 void test() {
