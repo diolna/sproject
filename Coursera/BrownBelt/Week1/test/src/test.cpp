6490d2a99deaab30a73214fdfacfc1a061fcdcf3
Dmitriy Kovalenko <kovalenkodmitrij@gmail.com>
2019-05-13 07:52:32 +0300

    set iterator next nOk

--- Coursera/BrownBelt/Week1/test/src/test.cpp
+++ Coursera/BrownBelt/Week1/test/src/test.cpp
@@ -7,6 +7,8 @@
 //============================================================================
 
 #include <iostream>
+#include <map>
+#include <unordered_map>
 using namespace std;
 
 int main() {
@@ -14,6 +16,14 @@ int main() {
 	int a = 28;
 	int b = 3;
 	cout << a%b << endl;
+
+	map<int, int> m = {{1,2},{2,3},{4,5},{6,7},{7,8}};
+	unordered_map<int, int> um = {{1,2},{2,3},{4,5},{6,7},{7,8}};
+	auto itmap = m.begin();
+	auto itumap = um.begin();
+//	auto it = itmap +3;
+	auto it2 = itumap + 3;
+
 
 	return 0;
 }
