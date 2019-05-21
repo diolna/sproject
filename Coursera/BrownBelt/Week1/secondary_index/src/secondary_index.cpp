eaf96f740c8d8e8a100fa7e9fa8db559be5443af
Dmitriy Kovalenko <kovalenkodmitrij@gmail.com>
2019-05-21 18:10:09 +0300

    ok

--- Coursera/BrownBelt/Week1/secondary_index/src/secondary_index.cpp
+++ Coursera/BrownBelt/Week1/secondary_index/src/secondary_index.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 #include <unordered_map>
+#include <tuple>
 
 
 
@@ -17,105 +18,34 @@ struct Record {
   int karma;
 
   bool operator==(const Record& other) {
-	  return tie(id,title, user, timestamp, karma) == tie(id, title, user, timestamp, karma);
+	  return tie(id,title, user, timestamp, karma)
+			  == tie(other.id, other.title, other.user, other.timestamp, other.karma);
+
   }
+  bool operator<(const Record& other) const {
+	  return id < other.id;
+  }
+
 };
 
  //Реализуйте этот класс
-//class Database {
-//public:
-//  bool Put(const Record& record) {
-//	  if(base.count(record.id) == 1) return false;
-//	  base[record.id] =  record;
-//	  int karma = record.karma;
-//	  pair< const int, Record> mypair = {karma, record};
-//	  rangebykarma.insert(mypair);
-//	  pair<const string, Record> myuspair = {record.user, record};
-//	  user_.insert(myuspair);
-//	  pair<const int, Record> mypairtime = {record.timestamp, record};
-//	  timestampbase.insert(mypairtime);
-//	  return true;
-//  }
-//  const Record* GetById(const string& id) const{
-//	  if(base.count(id) == 0 ) return nullptr;
-//	  return &base.at(id);
-//  }
-//  bool Erase(const string& id){
-//	  multimap<int, Record>::iterator it_temp;
-//	  multimap<int, Record>::iterator it_temp1;
-//	  multimap<string, Record>::iterator it_us;
-//	  if(base.count(id) == 1) {
-//		  Record temp = base.at(id);
-//		  base.erase(id);
-//		  for(auto it = timestampbase.begin(); it!=timestampbase.end(); ++it) {
-//			  if(it->second == temp) { it_temp = it;}
-//		  }
-//		  timestampbase.erase(it_temp);
-//		  for(auto it1 = user_.begin(); it1!=user_.end(); ++it1) {
-//		  			  if(it1->second == temp) { it_us = it1;}
-//		  		  }
-//		  user_.erase(it_us);
-//		  for(auto it = rangebykarma.begin(); it!=rangebykarma.end(); ++it) {
-//		  			  if(it->second == temp) { it_temp1 = it;}
-//		  		  }
-//		  rangebykarma.erase(it_temp1);
-//		  return true;
-//	  }
-//	  return false;
-//  }
-//
-//  template <typename Callback>
-//  void RangeByTimestamp(int low, int high, Callback callback) const{
-//	  for(int i = low; i<=high; ++i){
-//	  		  if(timestampbase.count(i) == 1) {
-//	  			  const Record& r = (timestampbase.find(i))->second;
-//	  			  if(callback(r) == false )  return;
-//	  		  }
-//	  	  }
-//  }
-//
-//  template <typename Callback>
-//  void RangeByKarma(int low, int high, Callback callback) const {
-//
-//	  for(int i = low; i<=high; ++i){
-//		  if(rangebykarma.count(i) == 1) {
-//			  const Record& r = (rangebykarma.find(i))->second;
-//			  if(callback(r) == false )  return;
-//		  }
-//	  }
-//  }
-//
-//  template <typename Callback>
-//  void AllByUser(const string& user, Callback callback) const {
-//	  auto [it, end] = user_.equal_range(user);
-//	  //auto it = range.first;
-//	  if(it != end){
-//		  while(it != end && callback(it->second)){
-//		  	  ++it;
-//		  }
-//	  }
-//  }
-//
-//private:
-//  unordered_map<string, Record> base;
-//  multimap<int, Record> timestampbase;
-//  multimap<string, Record> user_;
-//  multimap<int, Record> rangebykarma;
-//
-//};
-
 class Database {
 public:
+	using iterator = tuple<multimap<int, const Record*>::iterator
+				, multimap<string, const Record*>::iterator
+				, multimap< int, const Record*>::iterator>;
+
   bool Put(const Record& record) {
-	  if(base.count(record.id) == 1) return false;
-	  base[record.id] =  record;
-	  int karma = record.karma;
-	  pair< const int, Record> mypair = {karma, record};
-	  rangebykarma.insert(mypair);
-	  pair<const string, Record> myuspair = {record.user, record};
-	  user_.insert(myuspair);
-	  pair<const int, Record> mypairtime = {record.timestamp, record};
-	  timestampbase.insert(mypairtime);
+	  if(base.count(record.id) == 1) {
+		  return false;
+	  }
+	  base[record.id] =  record;  // вставляем базу
+	  const Record* rec = &base.at(record.id);
+	  auto it_1 = rangebykarma.insert({record.karma, rec});  // вставляем Karma
+	  auto it_2 = user_.insert({record.user, rec});    // вставляем user
+	  auto it_3 = timestampbase.insert({record.timestamp, rec});  // вставляем timestamp
+	  auto t = make_tuple(it_1, it_2, it_3);
+	  iterators.insert({rec, t});
 	  return true;
   }
   const Record* GetById(const string& id) const{
@@ -123,21 +53,24 @@ public:
 	  return &base.at(id);
   }
   bool Erase(const string& id){
-	  auto it =id_s.find(id);
-	  if(it == id_s.end()){
-		  return false;
+	   if(base.count(id) == 1) {
+		   const Record* temp = &base.at(id);
+		   auto element = iterators.find(temp);
+		   rangebykarma.erase(get<0>(element->second));
+		   user_.erase((get<1>(element->second)));
+		   timestampbase.erase(get<2>(element->second));
+		   iterators.erase(temp);
+ 		   base.erase(id);
+		   return true;
 	  }
-	  size_t index = it->second;
-	  auto el = iterators[index];
-	  timestamp_s.erase(get<0>(el));
-	  users
+	  return false;
   }
 
   template <typename Callback>
   void RangeByTimestamp(int low, int high, Callback callback) const{
-	  auto it = timestamp_s.lower_bound(low);
-	  	  auto end = timestamp_s.upper_bound(high);
-	  	  while(it != end && callback(records.at(it->second))){
+	  auto it = timestampbase.lower_bound(low);
+	  auto end = timestampbase.upper_bound(high);
+	  while(it != end && callback(*(it->second))){
 	  		  ++it;
 	  	  }
   }
@@ -145,33 +78,120 @@ public:
   template <typename Callback>
   void RangeByKarma(int low, int high, Callback callback) const {
 
-	  auto it = karma_s.lower_bound(low);
-	  auto end = karma_s.upper_bound(high);
-	  while(it != end && callback(records.at(it->second))){
-		  ++it;
-	  }
+	  auto it = rangebykarma.lower_bound(low);
+	  auto end = rangebykarma.upper_bound(high);
+	  while(it != end && callback(*(it->second))){
+	  		  ++it;
+	  	  }
   }
 
   template <typename Callback>
   void AllByUser(const string& user, Callback callback) const {
-	  auto [it, end] = users.equal_range(user);
+	  auto [it, end] = user_.equal_range(user);
 	  //auto it = range.first;
 	  if(it != end){
-		  while(it != end && callback(records.at(it->second))){
+		  while(it != end && callback(*(it->second))){
 		  	  ++it;
 		  }
 	  }
   }
 
 private:
-unordered_map<string, size_t> id_s;
-unordered_map<size_t, Record> records;
-multimap<int,size_t> timestamp_s;
-multimap<string,size_t> users;
-multimap<int, size_t> karma_s;
-size_t count =0;
-unordered_map<size_t, iterator> iterators;
+  unordered_map<string, Record> base;
+  multimap<int, const Record*> timestampbase;
+  multimap<string, const Record*> user_;
+  multimap<int, const Record*> rangebykarma;
+  multimap<const Record*, iterator> iterators;
+
 };
+
+//class Database {
+//public:
+//	using iterator = tuple<multimap<int, size_t>::iterator
+//			, multimap<string, size_t>::iterator
+//			, multimap< int, size_t>::iterator>;
+//
+//  bool Put(const Record& record) {
+//	  auto it = id_s.find(record.id);
+//	  if(it != id_s.end()){
+//		  return false;
+//	  }
+//	  id_s.insert({record.id, count});
+//	  auto it_1 = timestamp_s.insert({record.timestamp, count});
+//	  auto it_2 = users.insert({record.user, count});
+//	  auto it_3 = karma_s.insert({record.karma, count});
+//	  auto t = make_tuple(it_1, it_2, it_3);
+//	  iterators.insert({count, t});
+//	  records.insert({count++, record});
+//	  return true;
+//
+//
+//  }
+//  const Record* GetById(const string& id) const{
+//  	  auto it = id_s.find(id);
+//  	  if(it== id_s.end()){
+//  		  return nullptr;
+//  	  }
+//  	  return &records.at(it->second);
+//    }
+//
+//
+//  bool Erase(const string& id){
+//	  auto it =id_s.find(id);
+//	  if(it == id_s.end()){
+//		  return false;
+//	  }
+//	  size_t index = it->second;
+//	  auto el = iterators[index];
+//	  timestamp_s.erase(get<0>(el));
+//	  users.erase(get<1>(el));
+//	  karma_s.erase(get<2>(el));
+//	  records.erase(index);
+//	  iterators.erase(index);
+//	  id_s.erase(it);
+//	  return true;
+//
+//  }
+//
+//  template <typename Callback>
+//  void RangeByTimestamp(int low, int high, Callback callback) const{
+//	  auto it = timestamp_s.lower_bound(low);
+//	  	  auto end = timestamp_s.upper_bound(high);
+//	  	  while(it != end && callback(records.at(it->second))){
+//	  		  ++it;
+//	  	  }
+//  }
+//
+//  template <typename Callback>
+//  void RangeByKarma(int low, int high, Callback callback) const {
+//
+//	  auto it = karma_s.lower_bound(low);
+//	  auto end = karma_s.upper_bound(high);
+//	  while(it != end && callback(records.at(it->second))){
+//		  ++it;
+//	  }
+//  }
+//
+//  template <typename Callback>
+//  void AllByUser(const string& user, Callback callback) const {
+//	  auto [it, end] = users.equal_range(user);
+//	  //auto it = range.first;
+//	  if(it != end){
+//		  while(it != end && callback(records.at(it->second))){
+//		  	  ++it;
+//		  }
+//	  }
+//  }
+//
+//private:
+//unordered_map<string, size_t> id_s;
+//unordered_map<size_t, Record> records;
+//multimap<int,size_t> timestamp_s;
+//multimap<string,size_t> users;
+//multimap<int, size_t> karma_s;
+//size_t count =0;
+//unordered_map<size_t, iterator> iterators;
+//};
 
 void TestRangeBoundaries() {
   const int good_karma = 1000;
