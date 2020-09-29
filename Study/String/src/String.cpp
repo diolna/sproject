b2e96ae5ffea6264f6324dcaf8298b69222dd521
Dmitriy Kovalenko <kovalenkodmitrij@gmail.com>
2020-09-29 13:38:45 +0300

    add project String StivenPrata

---
+++ Study/String/src/String.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "string.h"
+
+const int ArSize = 10;
+const int MaxLen = 81;
+
+int main() {
+
+String name;
+std::cout << "Hi, whats your name?\n";
+std::cin >> name;
+std::cout << name << ", please enter up to " << ArSize << "short sayings <empty line to quit>:\n";
+
+String sayings[ArSize];
+char temp[MaxLen];
+int i;
+for(i =0; i < ArSize; i++){
+	std::cout << i + 1 << ": ";
+	std::cin.get(temp, MaxLen);
+	while(std::cin && std::cin.get() !='\n')
+		continue;
+	if(!std::cin || temp[0] == '\0')
+		break;
+	else
+		sayings[i] = temp;
+}
+int total = i;
+
+std::cout << "Here are your sayings:\n";
+for(i = 0; i < total; i ++)
+	std::cout << sayings[i] << "\n";
+
+int shortest = 0;
+int first = 0;
+for(i = 1; i < total; i++){
+	if(sayings[i].lenght() < sayings[shortest].lenght())
+		shortest = i;
+	if(sayings[i] < sayings[first])
+		first = 1;
+}
+
+std::cout << "Shortest saying:\n" << sayings[shortest] << "\n";
+std::cout << "First alphabetically:\n" << sayings[first] << "\n";
+std::cout << "This program used " << String::HowMany() << " String objects. Bye.\n";
+
+
+
+
+	return 0;
+}
+
